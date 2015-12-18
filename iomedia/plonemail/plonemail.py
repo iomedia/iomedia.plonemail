3060409d470ab37db5db6a265b7913a9d6db7d8a
Jon Pentland <jon@iomedia.co.uk>
2015-12-18 17:59:57 +0000

    Create outer multipart message.  Handle message attachments. PEP8 Cleanup.

--- iomedia/plonemail/plonemail.py
+++ iomedia/plonemail/plonemail.py
@@ -1,70 +1,82 @@
+import logging
+from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.encoders import encode_quopri
-from plone import api
-from Products.CMFCore.utils import getToolByName
-from zope.interface import Interface
 
 import html2text
+from plone import api
+from Products.CMFCore.utils import getToolByName
 
-import logging; logger=logging.getLogger('iomedia.plonemail')
+logger = logging.getLogger('iomedia.plonemail')
+
 
 class EmailBase():
 
-    def send_email(self):
-        transforms = getToolByName(self.context,'portal_transforms')
-        msg = MIMEMultipart('alternative')
+    attachments = []
 
+    def send_email(self):
+
+        # Build the outer message
+        msg = MIMEMultipart('related')
+        msg.preamble = 'This is a multi-part message in MIME format.'
+        msg_from = ' '.join([self.sender['firstname'],
+                             self.sender['lastname']])
+        msg.add_header('From', '"%s" <%s>' % (msg_from, self.sender['email']))
+        msg_to = ' '.join([self.recipient['firstname'],
+                           self.recipient['lastname']])
+        msg.add_header('To', '"%s" <%s>' % (msg_to, self.recipient['email']))
+        msg.add_header('Subject', self.subject)
+        version = (api.portal.get().portal_quickinstaller.getProductVersion(
+            'iomedia.plonemail'))
+        msg.add_header('User-Agent', 'iomedia.plonemail/%s' % str(version))
+
+        # Create the message text part
         transformer = html2text.HTML2Text()
         transformer.ignore_images = True
-
         textval = transformer.handle(self.message)
-        text = MIMEText(textval,'plain','utf-8')
+        text = MIMEText(textval, 'plain', 'utf-8')
+        html = MIMEText(self.message, 'html', 'utf-8')
+        msg_text = MIMEMultipart('alternative')
+        msg_text.attach(text)
+        msg_text.attach(html)
+        msg.attach(msg_text)
 
-        html = MIMEText(self.message,'html','utf-8')
-        msg.attach(text)
-        msg.attach(html)
-        msg_from = ' '.join([
-            self.sender['firstname'],
-            self.sender['lastname']
-        ])
-        msg.add_header('From','"%s" <%s>' % (msg_from,self.sender['email']))
-        msg_to = ' '.join([
-            self.recipient['firstname'],
-            self.recipient['lastname']
-        ])
-        msg.add_header('To','"%s" <%s>' % (msg_to,self.recipient['email']))
-        msg.add_header('Subject',self.subject)
-        version = (
-            api.portal.get().portal_quickinstaller
-            .getProductVersion('iomedia.plonemail')
-        )
-        msg.add_header('User-Agent','iomedia.plonemail/%s' % str(version))
+        # Add any attachmets to the message
+        for attach in self.attachments:
+            attachment = MIMEApplication(attach['data'],
+                                         _subtype=attach['ctype'])
+            attachment.add_header('content-disposition', 'attachment',
+                                  filename=attach['filename'])
+            msg.attach(attachment)
+
+        # Send the message
         host = getToolByName(self.context, 'MailHost')
         try:
-            host.send(msg,immediate=True)
+            host.send(msg, immediate=True)
         except Exception as error:
             logger.error('Email could not be sent: %s' % error)
+
+    def add_attachment(self, filename, data, ctype):
+        """ Adds a file attachment to the email. """
+        self.attachments.append({
+            'filename': filename,
+            'data': data,
+            'ctype': ctype})
 
     def admin_property(self):
         portal = getToolByName(self.context, 'portal_url').getPortalObject()
         pprops = getToolByName(self.context, 'portal_properties')
         site_props = getToolByName(pprops, 'site_properties')
-        admin_name=(
+        admin_name = (
             site_props.getProperty('email_from_name')
             or
-            portal.getProperty('email_from_name')
-        )
-        admin_addr=(
+            portal.getProperty('email_from_name'))
+        admin_addr = (
             site_props.getProperty('email_from_address')
-            or
-            portal.getProperty('email_from_address')
-        )
-        return {
-            'firstname': admin_name,
-            'lastname' : '',
-            'email'    : admin_addr
-        }
+            or portal.getProperty('email_from_address'))
+        return {'firstname': admin_name,
+                'lastname': '',
+                'email': admin_addr}
 
     def site_name(self):
         portal = getToolByName(self.context, 'portal_url').getPortalObject()
@@ -73,6 +85,5 @@ class EmailBase():
         site_name = (
             site_props.getProperty('email_from_name')
             or
-            portal.getProperty('email_from_name')
-        )
+            portal.getProperty('email_from_name'))
         return site_name
